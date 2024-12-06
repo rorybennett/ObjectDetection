0e2ac36013481e5bd6d8988192baf51764c4ff7c
Rory <rorybennett123@gmail.com>
2024-12-06 11:26:42 +0000

    change Transformers from classes to functions

--- Transformers/Transformers.py
+++ Transformers/Transformers.py
@@ -9,33 +9,25 @@ import torch
 from torchvision.transforms import v2
 
 
-class TrainingTransformers:
-    def __init__(self, image_size=(600, 600)):
-        self.transformers = v2.Compose([
-            v2.Resize(image_size),
-            # Original transforms.
-            v2.RandomAffine(degrees=30, shear=15, translate=(0.1, 0.1), scale=(0.6, 1.2)),
-            v2.RandomHorizontalFlip(p=0.2),
-            v2.RandomErasing(0.5, scale=(0.02, 0.08)),
-            v2.ToDtype(torch.float32, scale=True),
-            v2.GaussianNoise(mean=0, sigma=0.2),
-            v2.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 2)),
-            v2.Grayscale(num_output_channels=3),
-            v2.ToImage()
-        ])
-
-    def get_transformers(self):
-        return self.transformers
+def get_training_transforms(image_size=(600, 600)):
+    return v2.Compose([
+        v2.Resize(image_size),
+        # Original transforms.
+        v2.RandomAffine(degrees=30, shear=15, translate=(0.1, 0.1), scale=(0.6, 1.2)),
+        v2.RandomHorizontalFlip(p=0.2),
+        v2.RandomErasing(0.5, scale=(0.02, 0.08)),
+        v2.ToDtype(torch.float32, scale=True),
+        v2.GaussianNoise(mean=0, sigma=0.2),
+        v2.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 2)),
+        v2.Grayscale(num_output_channels=3),
+        v2.ToImage()
+    ])
 
 
-class ValidationTransformers:
-    def __init__(self, image_size=(600, 600)):
-        self.transformers = v2.Compose([
-            v2.Resize(image_size),
-            v2.ToDtype(torch.float32, scale=True),
-            v2.Grayscale(num_output_channels=3),
-            v2.ToImage()
-        ])
-
-    def get_transformers(self):
-        return self.transformers
+def get_validation_transforms(image_size=(600, 600)):
+    return v2.Compose([
+        v2.Resize(image_size),
+        v2.ToDtype(torch.float32, scale=True),
+        v2.Grayscale(num_output_channels=3),
+        v2.ToImage()
+    ])
