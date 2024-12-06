c64ebbce5a06a6ae2886dbde0c84662e66c6deff
Rory <rorybennett123@gmail.com>
2024-12-06 10:01:37 +0000

    ProstateBladderDataset.py finalised

--- Datasets/ProstateBladderDataset.py
+++ Datasets/ProstateBladderDataset.py
@@ -5,8 +5,10 @@ will be stored in the root/images directory and labels in the root/labels direct
 """
 import os
 
+import numpy as np
 import torch
 from PIL import Image
+from matplotlib import pyplot as plt, patches
 from natsort import natsorted
 from torchvision import tv_tensors
 from torchvision.transforms.v2 import functional as F
@@ -15,13 +17,16 @@ from . import datasets_model_types
 
 
 class ProstateBladderDataset(torch.utils.data.Dataset):
-    def __init__(self, root, label_names=None, transforms=None, oversampling_factor=1, model_type=None, verbose=False):
+    def __init__(self, images_root, labels_root, label_names=None, transforms=None, oversampling_factor=1,
+                 model_type=None, verbose=False):
         """
-        Initialise the ProstateBladderDataset class. Set the dataset root directory, the transforms that are to be used
-        when __getitem__() is called, the type of model that will be accessing this dataset class, and perform minor
-        dataset validation. The model_type parameter is necessary as different models follow different conventions.
+        Initialise the ProstateBladderDataset class. Set the images' and labels' root directories, the transforms
+        that are to be used when __getitem__() is called, the type of model that will be accessing this dataset class,
+        and perform minor dataset validation. The model_type parameter is necessary as different models follow
+        different conventions.
 
-        :param root: Directory containing images and labels folders.
+        :param images_root: Directory containing images.
+        :param labels_root: Directory containing labels.
         :param label_names: List of names for associated labels.
         :param transforms: Transformers to be used on this dataset.
         :param oversampling_factor: Increase dataset size by this amount (oversampling).
@@ -30,19 +35,22 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
         """
 
         self.verbose = verbose
-        self.root = root
+        self.images_root = images_root
+        self.labels_root = labels_root
         self.label_names = label_names
         self.transforms = transforms
         self.oversampling_factor = oversampling_factor
         self.model_type = model_type
-        self.imgs = list(natsorted(os.listdir(os.path.join(root, "images"))))
-        self.labels = list(natsorted(os.listdir(os.path.join(root, "labels"))))
+        self.imgs = list(natsorted(os.listdir(self.images_root)))
+        self.labels = list(natsorted(os.listdir(self.labels_root)))
 
         if self.verbose:
-            print(f'Initialising the ProstateBladder dataset with root: {self.root}.\n'
-                  f'Total Images: {len(self.imgs)}.\n'
-                  f'Total Labels: {len(self.labels)}.\n'
-                  f'Oversampling count: {self.oversampling_factor}.')
+            print(f'Initialising the ProstateBladder dataset'
+                  f'\tImages root: {self.images_root}.\n'
+                  f'\tLabels root: {self.labels_root}.\n'
+                  f'\tTotal Images: {len(self.imgs)}.\n'
+                  f'\tTotal Labels: {len(self.labels)}.\n'
+                  f'\tOversampling count: {self.oversampling_factor}.')
 
         self.validate_dataset()
 
@@ -67,21 +75,46 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
         :return: The image and the targets dict, post transformation.
         """
         original_idx = idx // self.oversampling_factor
-        img_path = os.path.join(self.root, "images", self.imgs[original_idx])
-        label_path = os.path.join(self.root, "labels", self.labels[original_idx])
+        img_path = os.path.join(self.images_root, self.imgs[original_idx])
+        label_path = os.path.join(self.labels_root, self.labels[original_idx])
         img = Image.open(img_path).convert("RGB")
 
-        # Read the label file.
+        # Get targets (bounding boxes, labels, etc.)
+        target = self.get_targets(label_path, img.size, idx)
+
+        # Convert to the format expected by the model.
+        img = tv_tensors.Image(img)
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        # Total images = input images * oversampling factor.
+        return len(self.imgs) * self.oversampling_factor
+
+    def get_targets(self, label_path, img_size, idx):
+        """
+        Get target data from label_path file. Conversion from YOLO format is done here.
+
+        :param label_path: Path to label.txt file.
+        :param img_size: Size of image (width, height).
+        :param idx: Index of image.
+
+        :return: Target dict, varies based on model type.
+        """
+        # Read the label file assuming YOLO format.
         boxes = []
         labels = []
         with open(label_path) as f:
+            img_width, img_height = img_size
             for line in f:
                 parts = line.strip().split()
                 class_id = int(parts[0])
                 x_center, y_center, width, height = map(float, parts[1:])
 
                 # Convert from YOLO format to (x_min, y_min, x_max, y_max) format.
-                img_width, img_height = img.size
                 x_min = (x_center - width / 2) * img_width
                 y_min = (y_center - height / 2) * img_height
                 x_max = (x_center + width / 2) * img_width
@@ -90,12 +123,11 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
                 boxes.append([x_min, y_min, x_max, y_max])
 
                 if self.model_type == datasets_model_types['fasterrcnn_resnet50_fpn_v2']:
-                    # Add 1 as 0 is always background for fasterrcnn.
+                    # Add 1 since 0 is always background for fasterrcnn.
                     labels.append(class_id + 1)
                 else:
                     labels.append(class_id)
-        # Convert to the format expected by the model.
-        img = tv_tensors.Image(img)
+
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
         labels = torch.as_tensor(labels, dtype=torch.int64)
 
@@ -105,7 +137,7 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
             area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
             iscrowd = torch.zeros((len(boxes),), dtype=torch.int64)
             target = {
-                "boxes": tv_tensors.BoundingBoxes(boxes, format="XYXY", canvas_size=F.get_size(img)),
+                "boxes": tv_tensors.BoundingBoxes(boxes, format="XYXY", canvas_size=(img_height, img_width)),
                 "labels": labels,
                 "image_id": image_id,
                 "area": area,
@@ -113,19 +145,11 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
             }
         else:
             target = {
-                "boxes": tv_tensors.BoundingBoxes(boxes, format="XYXY", canvas_size=F.get_size(img)),
+                "boxes": tv_tensors.BoundingBoxes(boxes, format="XYXY", canvas_size=(img_height, img_width)),
                 "labels": labels
             }
 
-        if self.transforms is not None:
-            # Apply transforms. This proved problematic, if something seems off plot the before and after.
-            img, target = self.transforms(img, target)
-
-        return img, target
-
-    def __len__(self):
-        # Total images = input images * oversampling factor.
-        return len(self.imgs) * self.oversampling_factor
+        return target
 
     def get_label_name(self, label: int):
         """
@@ -163,3 +187,45 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
 
         if self.verbose:
             print(f'Dataset validation passed.')
+
+    def display_transforms(self, idx):
+        """
+        This function will plot the original image with bounding boxes and the transformed image with transformed
+        bounding boxes. Shear + Rotate transforms may make it seem like the transformed bounding boxes are out of place,
+        but it should be fine. Boxes are displayed using 1 of 4 colours, with no special order.
+
+        :param idx: Index of image.
+        """
+        original_idx = idx // self.oversampling_factor
+        img_path = os.path.join(self.images_root, self.imgs[original_idx])
+        label_path = os.path.join(self.labels_root, self.labels[original_idx])
+        img = Image.open(img_path).convert("RGB")
+
+        # Get targets (bounding boxes, labels, etc.)
+        target = self.get_targets(label_path, img.size, idx)
+
+        boxes = target['boxes']
+        img = tv_tensors.Image(img)
+
+        colours = ['g', 'b', 'r', 'magenta']
+        _, ax = plt.subplots(2)
+        # Show original image with original bounding boxes.
+        ax[0].imshow(np.transpose(img, (1, 2, 0)))
+        for index, b in enumerate(boxes):
+            patch = patches.Rectangle((b[0], b[1]), b[2] - b[0], b[3] - b[1], linewidth=1,
+                                      edgecolor=colours[index], facecolor='none')
+            ax[0].add_patch(patch)
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        boxes = target['boxes']
+
+        # Show transformed image with transformed bounding boxes.
+        ax[1].imshow(np.transpose(img, (1, 2, 0)))
+        for index, b in enumerate(boxes):
+            patch = patches.Rectangle((b[0], b[1]), b[2] - b[0], b[3] - b[1], linewidth=1,
+                                      edgecolor=colours[index % len(colours)], facecolor='none')
+            ax[1].add_patch(patch)
+        plt.show()
+
--- Datasets/__init__.py
+++ Datasets/__init__.py
@@ -1,3 +1,9 @@
+"""
+Dataset classes to be used by the various models.
+
+ProstateBladderDataset.py can be used as a starting point.
+"""
+
 datasets_model_types = {
     'fasterrcnn_resnet50_fpn_v2': 'fasterrcnn_resnet50_fpn_v2',
     'retinanet_resnet50_fpn_v2': 'retinanet_resnet50_fpn_v2'
