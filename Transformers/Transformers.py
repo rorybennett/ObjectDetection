bda18d4a803caa9ddf7f7b486f1aaeb8f6c619a9
Rory <rorybennett123@gmail.com>
2024-12-06 10:36:46 +0000

    update transforms

--- Transformers/Transformers.py
+++ Transformers/Transformers.py
@@ -1,7 +1,8 @@
 """
 Transforms used during training and validation.
 
-Can be altered to suit specific needs.
+Can be altered to suit specific needs. Since this project was created for ultrasound images, the main transforms
+deal with physical transformations, with only Gaussian transformations done on the "colour" level.
 """
 
 import torch
@@ -9,14 +10,18 @@ from torchvision.transforms import v2
 
 
 class TrainingTransformers:
-    def __init__(self, image_size=600):
+    def __init__(self, image_size=(600, 600)):
         self.transformers = v2.Compose([
-            v2.Resize((image_size, image_size)),
+            v2.Resize(image_size),
+            # Original transforms.
             v2.RandomAffine(degrees=30, shear=15, translate=(0.1, 0.1), scale=(0.6, 1.2)),
             v2.RandomHorizontalFlip(p=0.2),
             v2.RandomErasing(0.5, scale=(0.02, 0.08)),
-            v2.ToImage(),
-            v2.ToDtype(torch.float32, scale=True)
+            v2.ToDtype(torch.float32, scale=True),
+            v2.GaussianNoise(mean=0, sigma=0.2),
+            v2.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 2)),
+            v2.Grayscale(num_output_channels=3),
+            v2.ToImage()
         ])
 
     def get_transformers(self):
@@ -24,11 +29,12 @@ class TrainingTransformers:
 
 
 class ValidationTransformers:
-    def __init__(self, image_size=600):
+    def __init__(self, image_size=(600, 600)):
         self.transformers = v2.Compose([
-            v2.Resize((image_size, image_size)),
-            v2.ToImage(),
-            v2.ToDtype(torch.float32, scale=True)
+            v2.Resize(image_size),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Grayscale(num_output_channels=3),
+            v2.ToImage()
         ])
 
     def get_transformers(self):
