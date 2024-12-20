1360c22e7f8845e63f37a219e561de78460aefe6
Rory <rorybennett123@gmail.com>
2024-12-20 13:46:27 +0000

    quite a few changes, mainly fix tranform order

--- Datasets/ProstateBladderDataset.py
+++ Datasets/ProstateBladderDataset.py
@@ -5,19 +5,20 @@ will be stored in the root/images directory and labels in the root/labels direct
 """
 import os
 
+import cv2
 import numpy as np
 import torch
-from PIL import Image
 from matplotlib import pyplot as plt, patches
 from natsort import natsorted
 from torchvision import tv_tensors
+from torchvision.transforms import v2
 
 from . import model_fasterrcnn, model_retinanet
 
 
 class ProstateBladderDataset(torch.utils.data.Dataset):
-    def __init__(self, images_root, labels_root, label_names=None, transforms=None, oversampling_factor=1,
-                 model_type=None, verbose=False):
+    def __init__(self, images_root, labels_root, train_mean, train_std, image_size, label_names=None,
+                 optional_transforms=None, oversampling_factor=1, model_type=None, verbose=False):
         """
         Initialise the ProstateBladderDataset class. Set the images' and labels' root directories, the transforms
         that are to be used when __getitem__() is called, the type of model that will be accessing this dataset class,
@@ -26,8 +27,11 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
 
         :param images_root: Directory containing images.
         :param labels_root: Directory containing labels.
+        :param train_mean: Mean of the training set, used in normalising the training and validation data.
+        :param train_std: Standard deviation of the training set.
+        :param image_size: Final size of image before being given to the model.
         :param label_names: List of names for associated labels.
-        :param transforms: Transformers to be used on this dataset.
+        :param optional_transforms: Transformers to be used on this dataset.
         :param oversampling_factor: Increase dataset size by this amount (oversampling).
         :param model_type: String representation of the model type this dataset is used with.
         :param verbose: Print details to screen.
@@ -37,11 +41,20 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
         self.images_root = images_root
         self.labels_root = labels_root
         self.label_names = label_names
-        self.transforms = transforms
+        self.image_size = image_size
+        self.train_mean = train_mean
+        self.train_std = train_std
+        self.optional_transforms = optional_transforms
         self.oversampling_factor = oversampling_factor
         self.model_type = model_type
         self.imgs = list(natsorted(os.listdir(self.images_root)))
         self.labels = list(natsorted(os.listdir(self.labels_root)))
+        # Transforms that are required (resizing and normalising).
+        self.required_transforms = v2.Compose([
+            v2.Resize(self.image_size),
+            v2.Normalize([self.train_mean], [self.train_std]),
+            v2.Grayscale(num_output_channels=3)
+        ])
 
         if self.verbose:
             print(f'Initialising the ProstateBladder dataset'
@@ -49,6 +62,9 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
                   f'\tLabels root: {self.labels_root}.\n'
                   f'\tTotal Images: {len(self.imgs)}.\n'
                   f'\tTotal Labels: {len(self.labels)}.\n'
+                  f'\tImage Size: {self.image_size}.\n'
+                  f'\tTraining Dataset Mean: {self.train_mean}.\n'
+                  f'\tTraining Dataset Standard Deviation: {self.train_std}.\n'
                   f'\tOversampling count: {self.oversampling_factor}.')
 
         self.validate_dataset()
@@ -68,6 +84,9 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
                 - image_id: Unique image identifier.
                 - area: Area of the bounding boxes.
                 - iscrowd: Are the instances considered a crowd.
+        Reads the image and mask from the disk, converts them to relative tv_tensors,
+        applies necessary then optional transforms (optional only applied if over_sampling > 1), and can display
+        images and labels before and after transforms if verbose == True.
 
         :param idx: Index of the required image-label set.
 
@@ -76,16 +95,21 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
         original_idx = idx // self.oversampling_factor
         img_path = os.path.join(self.images_root, self.imgs[original_idx])
         label_path = os.path.join(self.labels_root, self.labels[original_idx])
-        img = Image.open(img_path).convert("RGB")
-
+        img = self._read_image(img_path)
         # Get targets (bounding boxes, labels, etc.)
-        target = self.get_targets(label_path, img.size, idx)
+        target = self.get_targets(label_path, img.shape, idx)
 
-        # Convert to the format expected by the model.
+        # Convert img to the format expected by the model.
+        img = torch.tensor(img, dtype=torch.float32)
         img = tv_tensors.Image(img)
 
-        if self.transforms is not None:
-            img, target = self.transforms(img, target)
+        # Apply optional transforms.
+        if self.optional_transforms:
+            # Only apply optional transforms on oversampled images.
+            if idx % self.oversampling_factor != 0:
+                img, target = self.optional_transforms(img, target)
+        # Apply required transforms.
+        img, target = self.required_transforms(img, target)
 
         return img, target
 
@@ -107,7 +131,7 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
         boxes = []
         labels = []
         with open(label_path) as f:
-            img_width, img_height = img_size
+            img_height, img_width = img_size
             for line in f:
                 parts = line.strip().split()
                 class_id = int(parts[0])
@@ -193,39 +217,94 @@ class ProstateBladderDataset(torch.utils.data.Dataset):
         """
         This function will plot the original image with bounding boxes and the transformed image with transformed
         bounding boxes. Shear + Rotate transforms may make it seem like the transformed bounding boxes are out of place,
-        but it should be fine. Boxes are displayed using 1 of 4 colours, with no special order.
+        but it should be fine. Boxes are displayed using 1 of 4 colours, with no special order. The original dataset
+        was greyscale, so the ax.imshow cmap is set to greyscale, and the output of the necessary transforms is a single
+        channel greyscale image, as opposed to a 3 channel greyscale image as use din the __getitem__() method.
 
         :param idx: Index of image.
         """
         original_idx = idx // self.oversampling_factor
         img_path = os.path.join(self.images_root, self.imgs[original_idx])
         label_path = os.path.join(self.labels_root, self.labels[original_idx])
-        img = Image.open(img_path).convert("RGB")
+        img = self._read_image(img_path)
 
         # Get targets (bounding boxes, labels, etc.)
-        target = self.get_targets(label_path, img.size, idx)
+        target = self.get_targets(label_path, img.shape, idx)
 
         boxes = target['boxes']
+        img = torch.tensor(img, dtype=torch.float32)
         img = tv_tensors.Image(img)
 
         colours = ['g', 'b', 'r', 'magenta']
         _, ax = plt.subplots(2)
         # Show original image with original bounding boxes.
-        ax[0].imshow(np.transpose(img, (1, 2, 0)))
+        ax[0].imshow(np.transpose(img, (1, 2, 0)), cmap='grey')
         for index, b in enumerate(boxes):
             patch = patches.Rectangle((b[0], b[1]), b[2] - b[0], b[3] - b[1], linewidth=1,
                                       edgecolor=colours[index], facecolor='none')
             ax[0].add_patch(patch)
 
-        if self.transforms is not None:
-            img, target = self.transforms(img, target)
+            # Apply optional transforms.
+            if self.optional_transforms is not None:
+                # Only apply optional transforms on oversampled images.
+                if idx % self.oversampling_factor != 0:
+                    img, target = self.optional_transforms(img, target)
+            # Apply required transforms (slightly altered from __getitem() as ax.imshow(cmap='gray') has limitations.
+            required_transforms = v2.Compose([
+                v2.Resize(self.image_size),
+                v2.Normalize([self.train_mean], [self.train_std]),
+                v2.Grayscale(num_output_channels=1)
+            ])
+            img, target = required_transforms(img, target)
 
         boxes = target['boxes']
 
         # Show transformed image with transformed bounding boxes.
-        ax[1].imshow(np.transpose(img, (1, 2, 0)))
+        ax[1].imshow(np.transpose(img, (1, 2, 0)), cmap='gray')
         for index, b in enumerate(boxes):
             patch = patches.Rectangle((b[0], b[1]), b[2] - b[0], b[3] - b[1], linewidth=1,
                                       edgecolor=colours[index % len(colours)], facecolor='none')
             ax[1].add_patch(patch)
         plt.show()
+
+    def get_mean_and_std(self):
+        """
+        Return the mean and std of the current dataset, scaled between (0, 1). The assumption is that the input images
+        are scaled to (0, 255), so if the mean and std seem wildly off just make sure the input pixels are scaled
+        correctly. These values are required for the normalising transform, and the training values should be used on
+        the validation dataset.
+
+        :return: dataset_mean, dataset_std.
+        """
+        means = []
+        stds = []
+        for image in self.imgs:
+            # Read image using cv2.
+            image = cv2.imread(os.path.join(self.images_root, image), cv2.IMREAD_GRAYSCALE) / 255
+            # Calculate mean and std using cv2.
+            m, s = cv2.meanStdDev(image)
+            means.append(m)
+            stds.append(s)
+
+        dataset_mean = np.mean(means)
+        dataset_std = np.mean(stds)
+
+        if self.verbose:
+            print(f'Calculated Dataset Mean: {dataset_mean}.\n'
+                  f'Calculated Dataset Standard Deviation: {dataset_std}.')
+
+        return dataset_mean, dataset_std
+
+    @staticmethod
+    def _read_image(img_path):
+        """
+        Moved to a separate function as more than one method makes use of this call, so a single change here will
+        subsequently update all other calls.
+
+        Images are read as greyscale, then the range is changed to [0, 1].
+
+        :param img_path: Path to image that must be read from disk.
+        :return:
+        """
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE) / 255
+        return img
