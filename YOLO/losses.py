88f25078cd4c0d590d797036be4ef2dc9a2bea76
Rory <rorybennett123@gmail.com>
2025-03-05 09:50:38 +0000

    formatting

--- YOLO/losses.py
+++ YOLO/losses.py
@@ -1,8 +1,6 @@
 import torch
 from torch import nn
 
-from Utils import yolo_utils
-
 
 class YOLOv1Loss(nn.Module):
     def __init__(self, S=7, B=2, C=1):
@@ -62,10 +60,6 @@ class YOLOv1Loss(nn.Module):
         xy_loss = self.mse(pred_best_boxes[..., :2],
                            target_best_boxes[..., :2])  # (x, y)
 
-        # wh_loss = self.mse(
-        #     torch.sqrt(torch.exp(pred_best_boxes[..., 2:4])),
-        #     torch.sqrt(torch.exp(target_best_boxes[..., 2:4]))
-        # )
         wh_loss = self.mse(
             torch.flatten(torch.sign(pred_best_boxes[..., 2:4]) * torch.sqrt(
                 torch.abs(pred_best_boxes[..., 2:4] + 1e-6)), end_dim=-2),
@@ -90,7 +84,8 @@ class YOLOv1Loss(nn.Module):
 
     def iou(self, box1, box2):
         """
-        Compute Intersection over Union (IoU) between two bounding boxes.
+        Compute Intersection over Union (IoU) between two bounding boxes. Keep all previous dimensions when selecting
+        box corner points so when you select the best IoU, you can get the rest of the data immediately.
 
         Args:
         - box1 (Tensor): (batch, S, S, B, 4) -> (x, y, w, h)
@@ -108,117 +103,14 @@ class YOLOv1Loss(nn.Module):
         box2_y1 = box2[..., 1] - box2[..., 3] / 2
         box2_x2 = box2[..., 0] + box2[..., 2] / 2
         box2_y2 = box2[..., 1] + box2[..., 3] / 2
-
+        # Intersection area box.
         inter_x1 = torch.max(box1_x1, box2_x1)
         inter_y1 = torch.max(box1_y1, box2_y1)
         inter_x2 = torch.min(box1_x2, box2_x2)
         inter_y2 = torch.min(box1_y2, box2_y2)
-
-        inter_area = torch.clamp(inter_x2 - inter_x1, min=0) * torch.clamp(inter_y2 - inter_y1, min=0)
+        # Clamp is for when there is no intersection.
+        intersection_area = torch.clamp(inter_x2 - inter_x1, min=0) * torch.clamp(inter_y2 - inter_y1, min=0)
         box1_area = (box1_x2 - box1_x1) * (box1_y2 - box1_y1)
         box2_area = (box2_x2 - box2_x1) * (box2_y2 - box2_y1)
 
-        return inter_area / (box1_area + box2_area - inter_area + 1e-6)
-
-
-class YOLOv1LossSpecial(nn.Module):
-    def __init__(self, S=7, B=2, C=1, lambda_noobj=0.5, lambda_coord=5):
-        super(YOLOv1LossSpecial, self).__init__()
-        self.mse = nn.MSELoss(reduction='sum')
-        self.S = S
-        self.B = B
-        self.C = C
-        self.lambda_noobj = lambda_noobj
-        self.lambda_coord = lambda_coord
-
-    def forward(self, predictions, target):
-        # predictions are shaped (BATCH_SIZE, S*S(C+B*5) when inputted
-        predictions = predictions.reshape(-1, self.S, self.S, self.C + self.B * 5)
-
-        # Calculate IoU for the two predicted bounding boxes with target bbox
-        iou_b1 = intersection_over_union(predictions[..., 21:25], target[..., 21:25])
-        iou_b2 = intersection_over_union(predictions[..., 26:30], target[..., 21:25])
-        ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim=0)
-
-        # Take the box with highest IoU out of the two prediction
-        # Note that bestbox will be indices of 0, 1 for which bbox was best
-        iou_maxes, bestbox = torch.max(ious, dim=0)
-        exists_box = target[..., 20].unsqueeze(3)  # in paper this is Iobj_i
-
-        # ======================== #
-        #   FOR BOX COORDINATES    #
-        # ======================== #
-
-        # Set boxes with no object in them to 0. We only take out one of the two
-        # predictions, which is the one with highest Iou calculated previously.
-        box_predictions = exists_box * (
-            (
-                bestbox * predictions[..., 26:30]
-                + (1 - bestbox) * predictions[..., 21:25]
-            )
-        )
-
-        box_targets = exists_box * target[..., 21:25]
-
-        # Take sqrt of width, height of boxes to ensure that
-        box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) * torch.sqrt(
-            torch.abs(box_predictions[..., 2:4] + 1e-6)
-        )
-        box_targets[..., 2:4] = torch.sqrt(box_targets[..., 2:4])
-
-        box_loss = self.mse(
-            torch.flatten(box_predictions, end_dim=-2),
-            torch.flatten(box_targets, end_dim=-2),
-        )
-
-        # ==================== #
-        #   FOR OBJECT LOSS    #
-        # ==================== #
-
-        # pred_box is the confidence score for the bbox with highest IoU
-        pred_box = (
-            bestbox * predictions[..., 25:26] + (1 - bestbox) * predictions[..., 20:21]
-        )
-
-        object_loss = self.mse(
-            torch.flatten(exists_box * pred_box),
-            torch.flatten(exists_box * target[..., 20:21]),
-        )
-
-        # ======================= #
-        #   FOR NO OBJECT LOSS    #
-        # ======================= #
-
-        #max_no_obj = torch.max(predictions[..., 20:21], predictions[..., 25:26])
-        #no_object_loss = self.mse(
-        #    torch.flatten((1 - exists_box) * max_no_obj, start_dim=1),
-        #    torch.flatten((1 - exists_box) * target[..., 20:21], start_dim=1),
-        #)
-
-        no_object_loss = self.mse(
-            torch.flatten((1 - exists_box) * predictions[..., 20:21], start_dim=1),
-            torch.flatten((1 - exists_box) * target[..., 20:21], start_dim=1),
-        )
-
-        no_object_loss += self.mse(
-            torch.flatten((1 - exists_box) * predictions[..., 25:26], start_dim=1),
-            torch.flatten((1 - exists_box) * target[..., 20:21], start_dim=1)
-        )
-
-        # ================== #
-        #   FOR CLASS LOSS   #
-        # ================== #
-
-        class_loss = self.mse(
-            torch.flatten(exists_box * predictions[..., :20], end_dim=-2,),
-            torch.flatten(exists_box * target[..., :20], end_dim=-2,),
-        )
-
-        loss = (
-            self.lambda_coord * box_loss  # first two rows in paper
-            + object_loss  # third row in paper
-            + self.lambda_noobj * no_object_loss  # forth row
-            + class_loss  # fifth row
-        )
-
-        return loss
+        return intersection_area / (box1_area + box2_area - intersection_area + 1e-6)
