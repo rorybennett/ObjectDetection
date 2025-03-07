8c700a7a67b3a415d7534341b09482f1a8cf505b
Rory <rorybennett123@gmail.com>
2025-03-07 08:44:19 +0000

    tidy up

--- Utils/general_utils.py
+++ Utils/general_utils.py
@@ -1,18 +1,11 @@
 import platform
-from collections import defaultdict
-from os.path import join
 
-import numpy as np
 import torch
-from matplotlib import pyplot as plt, patches
-
-from . import box_colours
-
 
 
 def get_device_name():
     """
-    Return the name of the device being used by torch (GPU name or CPU name.
+    Return the name of the device being used by torch (GPU name or CPU name).
 
     :return: Name of torch device.
     """
